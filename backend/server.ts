a3e845e509a510fb6c2d66841afeb10d16c9adbb
Manish Devrani <manishdevrani777@gmail.com>
2024-08-18 12:45:18 +0530

    FEAT: added db initialized backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,51 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express();
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 
-app.get("/", function (req, res) {
-  res.json({
-    message: "recieved"
-  })
-});
+import router from "./routes";
+import { errorResponse, Status } from './utils/api-response';
 
-app.listen(3000, function () {
-  console.log("listening on port 3000");
-});
+const PORT = process.env.PORT ?? 8000;
+
+async function startServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(helmet());
+  app.use(cookieParser());
+
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }));
+
+  app.use("/api/v1", router);
+
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    res.status(404).json(errorResponse({
+      status: Status.ERROR,
+      code: 404,
+      message: "404 not found"
+    }));
+  });
+
+  app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+    res.status(500).json(errorResponse({
+      status: Status.ERROR,
+      code: 500,
+      message: "Internal server error occured"
+    }));
+  });
+
+  app.listen(PORT, function () {
+    console.log("Started on port: ", PORT);
+  });
+}
+
+startServer();
