431857eb14c1c0d4ce5faca4d7a95ef4ed053688
Manish Devrani <manishdevrani777@gmail.com>
2024-08-18 19:08:13 +0530

    FEAT: added route to check login status

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth";
 import healthcheckRouter from "./healthcheck";
+import usersRouter from "./users";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/healthcheck", healthcheckRouter);
+router.use("/users", usersRouter);
 
 export default router;
--- backend/server.ts
+++ backend/server.ts
@@ -21,7 +21,7 @@ async function startServer() {
   app.use(cookieParser());
 
   app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true,
   }));
 
